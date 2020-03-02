009dcfc12515af38aa73e9447bc05276f76b5665
Wouter <w_doppie@hotmail.com>
2020-03-02 22:02:03 +0100

    components: first iteration

--- analysis/components.py
+++ analysis/components.py
@@ -2,14 +2,35 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-class ExperimentHandler:
-    def __init__(self):
-        pass
+from dataclasses import dataclass
 
+@dataclass
 class Thruster:
-    def __init__(self):
-        pass
+    A_t: float
 
+@dataclass
+class Propellant:
+    h: float
+    c_l: float
+    c_v: float
+    R: float
+    rho: float
+    Gamma: float
+
+@dataclass
 class Environment:
-    def __init__(self):
-        pass
+    T_0: float
+    l: float
+    d: float
+
+class OperationalEnvelope:
+    def __init__(
+            self, 
+            thruster:       Thruster,
+            propellant:     Propellant,
+            environment:    Environment
+            ):
+            
+        self.th = thruster
+        self.prop = propellant
+        self.env = environment
--- main.py
+++ main.py
@@ -1,3 +1,28 @@
-from analysis import constants as c, functions as f
+from analysis import constants as c, functions as f, components as comp
 
-print(c.A)
+prop = comp.Propellant(
+    c.h,
+    c.c_l,
+    c.c_v,
+    c.R,
+    c.rho,
+    c.Gamma
+)
+
+th = comp.Thruster(
+    c.A_t
+)
+
+env = comp.Environment(
+    c.T_0,
+    c.l,
+    c.d
+)
+
+op = comp.OperationalEnvelope(
+    th,
+    prop,
+    env
+)
+
+print(op)
