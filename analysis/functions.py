71f379cd2d7ce02a7438dd9c9a31c8bab1d22b70
wdoppenberg <wouterdoppenberg@gmail.com>
2020-05-04 15:23:21 +0200

    Added thruster quality functions

--- analysis/functions.py
+++ analysis/functions.py
@@ -24,3 +24,55 @@ def vdkh(g):
     Vanderkerckhoven function
     """
     return np.sqrt(g) * ((2/(g+1)) ** ((g+1)/(2*(g-1))))
+
+def CA_2D(theta_nd):
+    """
+    2D conical nozzle
+    """
+    return np.sin(theta_nd)/theta_nd
+
+
+def CA_3D(theta_nd):
+    """
+    3D conical nozzle
+    """
+    return (1+np.cos(theta_nd))/2
+
+
+def C_F_divloss(CA, C_F0, p_e, p_a, p_c, AR):
+    """
+    Divergence loss
+    """
+    return CA*C_F0 + (p_e/p_a - p_a/p_c)*AR
+
+
+def C_F_vis(AR, Re):
+    """
+    Viscous loss
+    """
+    return (17.6 * np.exp(0.0032*AR))/np.sqrt(0.773*Re)
+
+
+def R_mod(Re, R_curv, R_t):
+    """
+    Modified Reynolds number
+    """
+    return Re*np.sqrt(R_curv/R_t)
+
+
+def C_d_bdloss(g, Re, R_curv, R_t):
+    """
+    Throat boundary layer loss
+    """
+    A = ((g+1)/2)**(3/4) * ( (72-32*np.sqrt(6))/(3*(g+1)) + (4*np.sqrt(6))/3 )\
+            * (1/np.sqrt(R_mod(Re, R_curv, R_t)))
+    B = ((2*np.sqrt(2)*(g-1)*(g+2))/(3*np.sqrt(g+1)))*(1/R_mod(Re, R_curv, R_t))
+
+    return 1 - A + B
+
+
+def dp_pipe(f, L, D, rho, v):
+    """
+    Pressure loss pipe
+    """
+    return f*(L/(2*D))*rho*v**2
